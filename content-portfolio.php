d493737beae7c64df32da9a3d4c7e5f5f810d163
kazumi oki <ka7z11.oune@gmail.com>
2019-10-13 16:27:14 +0900

    色修正、php設置

--- content-portfolio.php
+++ content-portfolio.php
@@ -56,7 +56,7 @@
     <div class="row featurette">
       <div class="col-md-4 order-md-1 mb-4">
         <!-- <img src="..." alt="image" class="img-thumbnail"> -->
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto  img-thumbnail" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">photo</text></svg>
+        <img src="/wp-content/themes/wp_my_theme/img/my.jpg" class="img-thumbnail rounded" alt="image" />
       </div>
       <div class="col-md-4 order-md-2 mb-4">
         <p class="h1 mb-4">Profile</p>
